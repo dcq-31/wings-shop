ec67cf846da0905bd97bbdea0a6e85e7b24399d9
Darián Capote <dariancapoteq@gmail.com>
2023-05-17 14:59:02 -0400

    Config daisyui color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: '#e8d47a',
+          secondary: '#2ad39e',
+          accent: '#bf6de8',
+          neutral: colors.gray[500],
+          'base-100': '#FFFFFF',
+          info: '#63A9DE',
+          success: '#1FA898',
+          warning: '#BC9106',
+          error: '#F20D2B'
+        }
+      }
+    ]
+  },
+  plugins: [require('daisyui')]
 }
