a355fdaa3c09711e7fbb4680c53c40c7e0b1c36c
Darián Capote <dariancapoteq@gmail.com>
2023-05-17 15:00:28 -0400

    Create a generic svg icon component

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import type { IconSVG } from '@/helpers/icons'
+const props = defineProps<IconSVG>()
+</script>
+<template>
+  <svg v-bind="svg">
+    <path v-bind="path" />
+  </svg>
+</template>
