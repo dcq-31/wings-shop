2770da33d078ad7be5e3fe4653961a85d044e355
Darián Capote <dariancapoteq@gmail.com>
2023-05-20 00:36:49 -0400

    Add new icons to bottom navigation bar

--- src/components/LayoutBottomNav.vue
+++ src/components/LayoutBottomNav.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { useRoute, RouterLink, type RouteLocationRaw } from 'vue-router'
 import BaseIcon from '@/components/BaseIcon.vue'
-import { home, type IconSVG } from '@/helpers/icons'
+import { home, squares2x2, shoppingCart, bell, user } from '@/helpers/icons'
+import type { IconSVG } from '@/helpers/icons'
 import { ROUTES } from '@/router/names'
 
 interface Link {
@@ -13,37 +14,37 @@ interface IconLink extends Link {
   icon: IconSVG
 }
 
-const NAV_LINKS: Array<IconLink> = [
+const LINKS: Array<IconLink> = [
   { icon: home, label: 'Inicio', to: { name: ROUTES.HOME } },
-  { icon: home, label: 'Categorías', to: { name: '' } },
-  { icon: home, label: 'Carrito', to: { name: ROUTES.CARET } },
-  { icon: home, label: 'Notificaciones', to: { name: '' } },
-  { icon: home, label: 'Cuenta', to: { name: ROUTES.USER } }
+  { icon: squares2x2, label: 'Categorías', to: { name: '' } },
+  { icon: shoppingCart, label: 'Carrito', to: { name: ROUTES.CARET } },
+  { icon: bell, label: 'Notificaciones', to: { name: '' } },
+  { icon: user, label: 'Cuenta', to: { name: ROUTES.USER } }
 ]
 
 const route = useRoute()
 </script>
 
 <template>
-  <div class="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t">
-    <div class="grid h-full max-w-lg grid-cols-5 mx-auto pb-2">
+  <div class="fixed bottom-0 left-0 w-full bg-white border-t z-10">
+    <div class="flex max-w-md justify-center mx-auto">
       <RouterLink
         :to="link.to"
-        class="inline-flex flex-col items-center justify-center px-2 text-neutral"
-        v-for="(link, index) in NAV_LINKS"
+        class="p-2 flex flex-col relative justify-end text-neutral h-16 w-24"
+        v-for="(link, index) in LINKS"
         :key="`layout-bottom-nav-icon-${index}`"
       >
         <div
           class="p-2"
           :class="{
-            'ring-2 ring-white rounded-full bg-primary relative -top-5 shadow-md':
+            'ring-2 ring-white rounded-full w-min bg-primary -top-5 shadow-md absolute left-0 right-0 mx-auto z-20':
               link.to.name == route.name
           }"
         >
           <BaseIcon
             v-bind="link.icon"
-            class="w-6 h-6 mx-auto"
-            :class="{ 'text-white w-7 h-7': link.to.name == route.name }"
+            class="mx-auto"
+            :class="[link.to.name == route.name ? 'text-white w-6 h-6' : 'w-5 h-5']"
           />
         </div>
         <div class="text-xs text-center">{{ link.label }}</div>
--- src/helpers/icons.ts
+++ src/helpers/icons.ts
@@ -1,3 +1,18 @@
+export interface IconSVG {
+  svg: {
+    xmlns: string
+    fill: string
+    viewBox: string
+    'stroke-width': string
+    stroke: string
+  }
+  path: {
+    'stroke-linecap': string
+    'stroke-linejoin': string
+    d: string
+  }
+}
+
 export const home: IconSVG = {
   svg: {
     xmlns: 'http://www.w3.org/2000/svg',
@@ -13,17 +28,62 @@ export const home: IconSVG = {
   }
 }
 
-export interface IconSVG {
+export const shoppingCart: IconSVG = {
   svg: {
-    xmlns: string
-    fill: string
-    viewBox: string
-    'stroke-width': string
-    stroke: string
-  }
+    xmlns: 'http://www.w3.org/2000/svg',
+    fill: 'none',
+    viewBox: '0 0 24 24',
+    'stroke-width': '1.5',
+    stroke: 'currentColor'
+  },
   path: {
-    'stroke-linecap': string
-    'stroke-linejoin': string
-    d: string
+    'stroke-linecap': 'round',
+    'stroke-linejoin': 'round',
+    d: 'M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z'
+  }
+}
+
+export const user: IconSVG = {
+  svg: {
+    xmlns: 'http://www.w3.org/2000/svg',
+    fill: 'none',
+    viewBox: '0 0 24 24',
+    'stroke-width': '1.5',
+    stroke: 'currentColor'
+  },
+  path: {
+    'stroke-linecap': 'round',
+    'stroke-linejoin': 'round',
+    d: 'M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z'
+  }
+}
+
+export const squares2x2: IconSVG = {
+  svg: {
+    xmlns: 'http://www.w3.org/2000/svg',
+    fill: 'none',
+    viewBox: '0 0 24 24',
+    'stroke-width': '1.5',
+    stroke: 'currentColor'
+  },
+  path: {
+    'stroke-linecap': 'round',
+    'stroke-linejoin': 'round',
+    d: 'M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z'
+  }
+}
+
+export const bell: IconSVG = {
+  svg: {
+    xmlns: 'http://www.w3.org/2000/svg',
+    fill: 'none',
+    viewBox: '0 0 24 24',
+    'stroke-width': '1.5',
+    stroke: 'currentColor'
+  },
+  path: {
+    'stroke-linecap': 'round',
+    'stroke-linejoin': 'round',
+    d: 'M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0'
   }
 }
